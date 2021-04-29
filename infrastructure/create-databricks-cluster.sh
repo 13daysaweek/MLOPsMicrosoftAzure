b6d7ab784fe8fdd6d80ca5b75df6abc8726baa77
Christopher House <chhouse@microsoft.com>
2021-04-29 14:04:57 +0000

    script is now successfully creating a cluster :D

--- infrastructure/create-databricks-cluster.sh
+++ infrastructure/create-databricks-cluster.sh
@@ -19,9 +19,17 @@ DATABRICKS_TOKEN=$(echo $AAD_TOKEN_RESPONSE | jq -r ".access_token")
 #TODO: workspace and rg names need to be input parameters
 WORKSPACE_ROOT_URL=$(az databricks workspace show -n amdb -g AUTUMN-ML-DEPLOY | jq -r ".workspaceUrl")
 
+MANAGEMENT_TOKEN_RESPONSE=$(curl -s -X GET \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-d "grant_type=client_credentials&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&resource=https%3A%2F%2Fmanagement.core.windows.net%2F" \
+https://login.microsoft.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token)
+
+MANAGEMENT_TOKEN=$(echo $MANAGEMENT_TOKEN_RESPONSE | jq -r ".access_token")
+
 CREATE_CLUSTER_URL="https://$WORKSPACE_ROOT_URL/api/2.0/clusters/create"
 echo $CREATE_CLUSTER_URL
 
+#TODO: Needs to be an input param
 WORKERS=8
 
 #TODO: need input params for cluster name, spark version, node type, number of workers
@@ -35,5 +43,10 @@ CREATE_CLUSTER_BODY=$(jq -n --arg WORKERS "$WORKERS" '{
   "num_workers": $WORKERS|tonumber
 }')
 
-curl -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer $DATABRICKS_TOKEN' -d "$CREATE_CLUSTER_BODY" $CREATE_CLUSTER_URL
-
+#TODO: Need input parameter for workspace resource id
+curl -s -X POST \
+-H 'Content-Type: application/json' \
+-H "Authorization: Bearer $DATABRICKS_TOKEN" \
+-H "X-Databricks-Azure-SP-Management-Token: $MANAGEMENT_TOKEN" \
+-H 'X-Databricks-Azure-Workspace-Resource-ID: /subscriptions/20032276-4660-477d-aefe-8379f2ab7c0d/resourceGroups/AUTUMN-ML-DEPLOY/providers/Microsoft.Databricks/workspaces/amdb' \
+-d "$CREATE_CLUSTER_BODY" $CREATE_CLUSTER_URL
